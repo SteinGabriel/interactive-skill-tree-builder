2599ec963954d8f6ec8f6293008a5bd4111bb445
Gabriel Stein <steindleve@gmail.com>
2025-12-13 15:52:09 -0800

    add initial JSDoc types definition

---
+++ src/lib/types.js
@@ -0,0 +1,49 @@
+/**
+ * @typedef {'locked' | 'unlockable' | 'unlocked' | 'completed'} SkillStatus
+ */
+
+/**
+ * Status values that should be persisted. `unlockable` is derived from prerequisites.
+ * @typedef {'locked' | 'unlocked' | 'completed'} PersistedSkillStatus
+ */
+
+/**
+ * @typedef {Object} SkillNodeData
+ * @property {string} title
+ * @property {string} [description]
+ * @property {number} [cost]
+ * @property {number} [level]
+ * @property {SkillStatus} status
+ */
+
+/**
+ * @typedef {Object} PersistedSkillNodeData
+ * @property {string} title
+ * @property {string} [description]
+ * @property {number} [cost]
+ * @property {number} [level]
+ * @property {PersistedSkillStatus} status
+ */
+
+/**
+ * @typedef {Object} PersistedNode
+ * @property {string} id
+ * @property {{ x: number, y: number }} position
+ * @property {PersistedSkillNodeData} data
+ */
+
+/**
+ * @typedef {Object} PersistedEdge
+ * @property {string} id
+ * @property {string} source
+ * @property {string} target
+ */
+
+/**
+ * @typedef {Object} PersistedTreeState
+ * @property {number} version
+ * @property {PersistedNode[]} nodes
+ * @property {PersistedEdge[]} edges
+ */
+
+export {}
