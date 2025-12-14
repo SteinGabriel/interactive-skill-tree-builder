ec8703bb73c79479f8e53130b729d10e334ecbff
Gabriel Stein <steindleve@gmail.com>
2025-12-13 18:12:59 -0800

    add ToastProvider to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '@/styles/index.css'
 import App from '@/app/App.jsx'
+import { ToastProvider } from '@/components/ui/ToastProvider'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ToastProvider>
+      <App />
+    </ToastProvider>
   </StrictMode>,
 )
