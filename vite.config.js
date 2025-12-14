476fa1a1d58dfbd5ba5d782dfc284dd5660c0346
Gabriel Stein <steindleve@gmail.com>
2025-12-14 11:06:28 -0800

    add live app url to vite config

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react-swc'
 import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/interactive-skill-tree-builder/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+}))
