1c3d4e820c932694dff54a69b7429486e6f3d9bf
Gabriel Stein <steindleve@gmail.com>
2025-12-14 16:13:27 -0800

    create svg icon components

---
+++ src/components/ui/icons/CheckIcon.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+/**
+ * @param {React.ComponentPropsWithoutRef<'svg'>} props
+ */
+export function CheckIcon({ className, ...props }) {
+  return (
+    <svg
+      viewBox="0 0 20 20"
+      fill="currentColor"
+      aria-hidden="true"
+      className={className}
+      {...props}
+    >
+      <path
+        fillRule="evenodd"
+        d="M16.704 5.296a1 1 0 010 1.414l-7.5 7.5a1 1 0 01-1.414 0l-3.5-3.5a1 1 0 011.414-1.414l2.793 2.793 6.793-6.793a1 1 0 011.414 0z"
+        clipRule="evenodd"
+      />
+    </svg>
+  )
+}
+
+CheckIcon.propTypes = {
+  className: PropTypes.string,
+}
+
